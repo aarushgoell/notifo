22493589d5ac9e24b08a35de52ed5f1d4a72093a
Aarush Goel <aarushgoel2004@gmail.com>
2024-02-22 03:55:33 +0530

    step-3

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -7,55 +5,49 @@ import 'package:notifo/firebase_options.dart';
 import 'package:notifo/views/login_view.dart';
 import 'package:notifo/views/register_view.dart';
 import 'package:notifo/views/verify_email.dart';
-
-
-
+import 'dart:developer' as devtools show log;
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-       
-        colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 22, 19, 27)),
-        useMaterial3: true,
-      ),
-      home: const Homepage(),
-      routes: {
-        'login' : (context) => const LoginView(),
-          'register' : (context) => const Registerview(),
-        }
-    ), 
-    );
+  runApp(
+    MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: const Color.fromARGB(255, 34, 145, 214)),
+          useMaterial3: true,
+        ),
+        home: const Homepage(),
+        routes: {
+          'login': (context) => const LoginView(),
+          'register': (context) => const Registerview(),
+        }),
+  );
 }
+
 class Homepage extends StatelessWidget {
   const Homepage({super.key});
 
-   @override
+  @override
   Widget build(BuildContext context) {
-
     return FutureBuilder(
-        future: Firebase.initializeApp(
-              options: DefaultFirebaseOptions.currentPlatform,
-              ),
-        builder: (context, snapshot){
-          switch (snapshot.connectionState){
-            
-            case ConnectionState.done:
-           final user = FirebaseAuth.instance.currentUser;
-           print(user);
-           if(user != null){
-            if(user.emailVerified){
-             print('Email is verified');
+      future: Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+      ),
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.done:
+            final user = FirebaseAuth.instance.currentUser;
+            if (user != null) {
+              if (user.emailVerified) {
+                return const NotesView();
+              } else {
+                return const VerifyEmailView();
+              }
+            } else {
+              return const LoginView();
             }
-           else{
-            print(user);
-            return const VerifyEmailView();
-           }
-           }else{
-            return const LoginView();
-           }
-           return const Text('Done');
+
           //  print(user);
           //  if(user?.emailVerified?? false){
           //   // print("You are a verified user");
@@ -64,11 +56,78 @@ class Homepage extends StatelessWidget {
           //   // print("You need to verify your email first");
           //  return const VerifyEmailView();
           //  }
-        default:
-          return const CircularProgressIndicator();
-          }
-        },
-      );
+          default:
+            return const CircularProgressIndicator();
+        }
+      },
+    );
   }
 }
 
+enum MenuAction { logout }
+
+class NotesView extends StatefulWidget {
+  const NotesView({super.key});
+
+  @override
+  State<NotesView> createState() => _NotesViewState();
+}
+
+class _NotesViewState extends State<NotesView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Notes'),
+        actions: [
+          PopupMenuButton<MenuAction>(
+            onSelected: (value) async {
+              switch (value) {
+                case MenuAction.logout:
+                  final shouldLogout = await showLogOutDialog(context);
+                  if (shouldLogout) {
+                    await FirebaseAuth.instance.signOut();
+                    Navigator.of(context).pushNamedAndRemoveUntil('login', (_) => false,
+                    );
+                  }
+              }
+            },
+            itemBuilder: (context) {
+              return const [
+                PopupMenuItem<MenuAction>(
+                  value: MenuAction.logout,
+                  child: Text('Logout'),
+                ),
+              ];
+            },
+          )
+        ],
+      ),
+      body: const Text('Hello World'),
+    );
+  }
+}
+
+Future<bool> showLogOutDialog(BuildContext context) {
+  return showDialog<bool>(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+          title: const Text('Sign out'),
+          content: const Text('Are you sure you want to sign out?'),
+          actions: [
+            TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop(false);
+                },
+                child: const Text('Cancel')),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(true);
+              },
+              child: const Text('Log out'),
+            )
+          ]);
+    },
+  ).then((value) => value ?? false);
+}
