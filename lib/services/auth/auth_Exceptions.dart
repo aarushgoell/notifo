805cf82a1d9ca23a5e989685269a4bd14a81ae38
Aarush Goel <aarushgoel2004@gmail.com>
2024-02-23 01:27:45 +0530

    step-8

---
+++ lib/services/auth/auth_Exceptions.dart
@@ -0,0 +1,21 @@
+
+// login exceptions 
+class InvalidemailAuthException implements Exception{}
+
+class InvalidcredentialAuthException implements Exception{}
+
+
+// register exceptions 
+
+class WeakpasswordAuthException implements Exception{}
+
+class EmailalreadyinuseAuthException implements Exception{}
+
+// generic exceptions
+
+class GenericAuthException implements Exception{}
+
+class UserNotLoggedInAuthException implements Exception{}
+
+
+
